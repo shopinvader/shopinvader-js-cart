b01847b3d2f35d70a4eb290cecdf59522ba16ffd
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-03-22 11:43:12 +0100

    Add a few module-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 // Copyright (c) ACSONE SA/NV 2022
 
 export { Cart } from './cart.js';
+export { CartData } from './cartData.js';
+export { CartLineData } from './cartLineData.js';
+
+export { CartTransaction } from './cartTransaction.js';
+
 export { CartObserver } from './cartObserver.js';
+
+export { CartStorage } from './cartStorage.js';
 export { MemoryCartStorage } from './memoryCartStorage.js';
+export { WebStorageCartStorage } from './webStorageCartStorage.js';
