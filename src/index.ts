2211f5d08b2e5c85c0c0526d654db5515c464e6a
Thibault Rey <thibault.rey@akretion.com>
2026-02-06 17:16:30 +0100

    update deps

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,8 @@ export { CartData } from './cartData.js';
 export { CartLineData } from './cartLineData.js';
 
 export { CartTransaction } from './cartTransaction.js';
+export type { CartObserver } from './cartObserver.js';
 
-export { CartObserver } from './cartObserver.js';
-
-export { CartStorage } from './cartStorage.js';
+export type { CartStorage } from './cartStorage.js';
 export { MemoryCartStorage } from './memoryCartStorage.js';
 export { WebStorageCartStorage } from './webStorageCartStorage.js';
